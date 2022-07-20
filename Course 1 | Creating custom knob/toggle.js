53d824128a4426df4d9038915744a2c68d55bff3
art-abdulwadud <abdulwadudhajike@gmail.com>
2022-07-20 07:47:44 +0300

    .

--- Course 1 | Creating custom knob/toggle.js
+++ Course 1 | Creating custom knob/toggle.js
@@ -1,4 +1,3 @@
-const knobContainer = document.querySelector('.knob-container');
 const circle = document.querySelector('.circle');
 
 circle.addEventListener('click', () => circle.classList.toggle('switched-on'));
