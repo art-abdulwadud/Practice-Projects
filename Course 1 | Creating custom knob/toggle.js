b0d2abd628ec6f6ac4d22e82b995315109ebfd35
art-abdulwadud <abdulwadudhajike@gmail.com>
2022-07-20 07:45:44 +0300

    move circle on clicking it

---
+++ Course 1 | Creating custom knob/toggle.js
@@ -0,0 +1,4 @@
+const knobContainer = document.querySelector('.knob-container');
+const circle = document.querySelector('.circle');
+
+circle.addEventListener('click', () => circle.classList.toggle('switched-on'));
