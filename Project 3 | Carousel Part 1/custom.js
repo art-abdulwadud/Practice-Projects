a1a91bdec77368287798a18cdbefd7f416d1205d
art-abdulwadud <abdulwadudhajike@gmail.com>
2022-07-21 21:35:10 +0300

    add more comments in custom js file

--- Project 3 | Carousel Part 1/custom.js
+++ Project 3 | Carousel Part 1/custom.js
@@ -1,6 +1,9 @@
+// Get the img tag for the image on display
 const imageOnDisplay = document.querySelector('.image-on-display img');
-const imageList = document.querySelectorAll('.image-list img'); // Get all the img tags in image-list div
+// Get all the img tags in image-list div
+const imageList = document.querySelectorAll('.image-list img'); // This is an array
 
+// Let's loop through the img tags in image-list div
 imageList.forEach((image) => {
     // Add an 'mouseover' event listener on each image in image list
     image.addEventListener('mouseover', (ev) => {
